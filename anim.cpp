71159ba4291d933d5b01a887648e722019ff9faf
dogolpeezon <dogolpeezon@gmail.com>
2018-02-20 10:30:30 +0000

    Update anim.cpp

--- anim.cpp
+++ anim.cpp
@@ -5,28 +5,17 @@ Anim::Anim( bool run ):
 _run( run ), g_window( glfwGetCurrentContext()),
 _render( &Render::getRender()),
 _input( &Input::getInput()){}
-// _shader( &Shader::getShader() )
-
 Anim::~Anim() { Render::destroyRender(); Input::destroyInput(); }
-// Shader::destroyShader();
 
 void Anim::loop (){
-    std::cout << "In loop.." << std::endl;
-
     while ( _run ){
         _run = !glfwWindowShouldClose( g_window );
-        //_input->update();
         glfwSetKeyCallback( g_window, Input::keyCallback );
         _render->render();
     }
-    std::cout << "Exit render.." << std::endl;
-    std::cout << "Exit loop.." << std::endl;
-
 }
 
 Anim &Anim::getAnim (){
-    std::cout << "Enter getAnim.." << std::endl;
-
     static Anim *anim = NULL;
     if ( anim == NULL ){
         glfwInit();
@@ -38,8 +27,6 @@ Anim &Anim::getAnim (){
 }
 
 void Anim::destroyAnim (){
-    std::cout << "Enter destroyAnim.." << std::endl;
-
     Anim *anim = &getAnim();
     delete anim;
     GLFWwindow *g_window = glfwGetCurrentContext();
